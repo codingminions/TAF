c5b1283c664dd76e847ba4ce568c51b1ad837704
Prateek Kumar <prateekkumar276@gmail.com>
2019-05-27 12:17:35 +0530

    Update basic_ops.py

--- pytests/Atomicity/basic_ops.py
+++ pytests/Atomicity/basic_ops.py
@@ -122,7 +122,7 @@ class basic_ops(BaseTestCase):
                                              commit=self.transaction_commit,durability=self.durability_level)
         print "going to execute the task"
         self.sleep(40)
-        self.task.jython_task_manager.get_task_result(task)
+        self.task_manager.get_task_result(task)
         
         if self.op_type == "time_out": 
             self.sleep(200, "sleep for 90 seconds so that the staged docs will be cleared")
