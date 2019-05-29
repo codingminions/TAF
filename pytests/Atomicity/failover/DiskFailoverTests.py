c1bc223ba18627763c7512c26e2322b808f9da2f
Prateek Kumar <prateekkumar276@gmail.com>
2019-05-29 11:51:25 +0530

    Update DiskFailoverTests.py

--- pytests/Atomicity/failover/DiskFailoverTests.py
+++ pytests/Atomicity/failover/DiskFailoverTests.py
@@ -15,8 +15,10 @@ class DiskAutofailoverTests(DiskAutoFailoverBasetest):
         tasks = []
         tasks.append(self.async_load_all_buckets(self.run_time_create_load_gen,
                                                  "create", 0))
+        self.sleep(10)
         tasks.append(self.async_load_all_buckets(self.update_load_gen,
                                                  "update", 0))
+        self.sleep(10)
         tasks.append(self.async_load_all_buckets(self.delete_load_gen,
                                                  "delete", 0))
         return tasks
